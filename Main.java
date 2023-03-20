21e86faa2f181b219887ec71e9be23b1cc2c6b3f
udhayakumar v n <104062640+umeshudhaya@users.noreply.github.com>
2023-03-20 20:21:03 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,12 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int num=0;
-        while (num<=20)
+        try {
+            int age=150;
+            if (age<0||age>=100)
+                throw new Exception();
+            if (age>=18 )
+            {
+                System.out.println("eligible to vote");
+            }
+            else {
+                System.out.println("not eligible to vote");
+            }
+        }catch (Exception e)
         {
-
-            System.out.println("* ");
-            num++;
+            System.out.println("some problem ,contact udhaya");
         }
     }
 }
