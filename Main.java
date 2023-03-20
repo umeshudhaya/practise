8240546ba27890d4be48e73c4de0261b851c6b30
udhayakumar v n <104062640+umeshudhaya@users.noreply.github.com>
2023-03-20 20:18:35 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,22 +1,15 @@
 package com.company;
 
-import java.util.Random;
-
 public class Main {
-    static   char[] otp(int len){
-        System.out.println("Generating the OTP using  Random() :");
-        System.out.print("Your OTP is.....:   ");
-        String num="0123456789";
-        Random ran=new Random();
-        char otp[]= new char[len];
-        for (int i = 0; i <len ; i++) {
-            otp[i]=num.charAt(ran.nextInt(num.length()));
-        }
-        return otp;
-    }
+
     public static void main(String[] args) {
 	// write your code here
-        int length=6;
-        System.out.println(otp(length));
+        int num=0;
+        while (num<=20)
+        {
+
+            System.out.println("* ");
+            num++;
+        }
     }
 }
